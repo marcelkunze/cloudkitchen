537f2f7d35d289dc1d08a8260a4b3f7492437f72
Marcel Kunze <marcel.kunze@googlemail.com>
2014-01-25 16:25:32 +0100

    First Version

---
+++ KlimaGAE/src/klima/GegenwartServlet.java
@@ -0,0 +1,17 @@
+package klima;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GegenwartServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Klimasmulation");
+
+		Klima gegenwart = new Klima("Gegenwart",280.,320.,0.30,0.11,0.31,0.53,0.06);
+		gegenwart.berechne();
+		resp.getWriter().println(gegenwart.name()+":"+gegenwart.temperatur());
+	}
+}
