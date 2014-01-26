846ce47a885bc2dfeaf5f5ac511ad0391bbf7131
Marcel.Kunze <marcel.kunze@gmail.com>
2014-01-26 18:26:24 +0100

    new

--- KlimaGAE/src/klima/GegenwartServlet.java
+++ KlimaGAE/src/klima/GegenwartServlet.java
@@ -1,12 +1,32 @@
 package klima;
 
 import java.io.IOException;
-import javax.servlet.http.*;
 
-@SuppressWarnings("serial")
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import klima.Klima;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
 public class GegenwartServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-	throws IOException {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GegenwartServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Klimasmulation");
 
@@ -14,4 +34,12 @@ public class GegenwartServlet extends HttpServlet {
 		gegenwart.berechne();
 		resp.getWriter().println(gegenwart.name()+":"+gegenwart.temperatur());
 	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
 }
